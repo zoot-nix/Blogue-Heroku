989a2b8a512dfaac08df3aad0f1fd05cca762cb9
Owais Shaikh <66899497+zoot-nix@users.noreply.github.com>
2022-02-23 19:10:57 +0400

    Update urls.py

--- blogue/urls.py
+++ blogue/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf.urls import url
+from django.conf.urls import url
 from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
@@ -15,8 +15,8 @@ urlpatterns = [
     path('', include('blogueApp.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
