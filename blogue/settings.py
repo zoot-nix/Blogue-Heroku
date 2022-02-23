3287a7e0d9b0147d36a44b286a5137e9620ff30d
Owais Shaikh <66899497+zoot-nix@users.noreply.github.com>
2022-02-23 19:38:14 +0400

    Update settings.py

--- blogue/settings.py
+++ blogue/settings.py
@@ -130,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'static'),
 #]
+DISABLE_COLLECTSTATIC=1
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
